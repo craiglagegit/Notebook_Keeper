ba5cd5de4412ee523b6737f02ad2ea86e31af92a
craiglagegit <cslage@ucdavis.edu>
2023-04-10 18:40:57 +0000

    Update 10Apr23

--- copy_notebooks_mac.py
+++ copy_notebooks_mac.py
@@ -1,4 +1,5 @@
 import sys, os, time
+from pathlib import Path
 from subprocess import Popen
 # This code strips the data from the notebooks, and copies them over for pushing to github.
 
@@ -6,15 +7,24 @@ dirs = [["/Users/cslage/.ipython/notebooks/auxtel_stuff", "/Users/cslage/Researc
 
 for [get_dir, put_dir] in dirs:
         files = os.listdir(get_dir)
+        filesToStore = []
         for file in files:
-                thisFile = f"{get_dir}/{file}"
                 if file.split(".")[-1] == "ipynb":
-                        print("Stripping and copying ", thisFile)
-                        command = f"jupyter nbconvert --to notebook --ClearOutputPreprocessor.enabled=True \
-                        --output-dir={put_dir} {thisFile}"
-                        strip_and_copy = Popen(command, shell=True)
-                        Popen.wait(strip_and_copy)
-                else:
-                        print("Skipping ", thisFile)
-
+                        thisFile = f"{get_dir}/{file}"
+                        storedFile = f"{put_dir}/{file}"
+                        thisPath = Path(thisFile)
+                        thisLastModified = thisPath.stat().st_mtime
+                        try:
+                                storedPath = Path(storedFile)
+                                storedLastModified = storedPath.stat().st_mtime
+                        except:
+                                filesToStore.append(thisFile)
+                        if thisLastModified > storedLastModified:
+                                filesToStore.append(thisFile)
+        for thisFile in filesToStore:
+                print("Stripping and copying ", thisFile)
+                command = f"jupyter nbconvert --to notebook --ClearOutputPreprocessor.enabled=True \
+                --output-dir={put_dir} {thisFile}"
+                strip_and_copy = Popen(command, shell=True)
+                Popen.wait(strip_and_copy)
 print("Done")
